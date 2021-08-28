951d75d90eb63ed20361b912e68447e41d8552ba
invaleed <invaleed@gmail.com>
2021-08-29 01:49:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,11 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 8080;
 
-console.log(process.env.ENVIRONMENT || 'e2e');
-
-if (process.env.ENVIRONMENT === 'prod') {	
-    process.exit(1);	
-}
-
-const server = http.createServer((_, res) => {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Amikom Yogyakarta!\n');
+  res.end('Hello, World!\n');
 });
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
